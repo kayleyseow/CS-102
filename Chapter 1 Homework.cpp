b575bf425d4ae0fb2dcedb988978b27f6d561adb
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-03 20:03:53 -0700

    Add files via upload

---
+++ Chapter 1 Homework.cpp
@@ -0,0 +1,15 @@
+// Kayley Seow
+// 9/3/2020
+// Lab 1
+// P1.7, This program prints my name in the middle of a box made using +,-, and |.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "+----------+"<<endl;
+    cout << "|  Kayley  |" << endl;
+    cout << "+----------+" << endl;
+    return 0;
+}
