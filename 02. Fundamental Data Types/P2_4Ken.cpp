c4100e866b5f67ab492055e0a5e6e698ecc7c752
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-07 00:44:43 -0700

    Create P2_4Ken.cpp
    Ken is the best teacher 😤

---
+++ 02. Fundamental Data Types/P2_4Ken.cpp
@@ -0,0 +1,13 @@
+//Thank you so much for teaching me, Ken!
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout
+        << "Sum: " << a + b
+        << "\nDifference: " << a - b
+        << "\nProduct: " << a * b
+        << "\nAverage: " << (a + b) / 2
+        << '\n';
+}
