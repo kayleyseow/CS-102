86e92db247ed033203e02aa3fbc3c59189e0e37e
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-08 00:30:08 -0700

    Create P2_20.cpp

---
+++ 02. Fundamental Data Types/P2_20.cpp
@@ -0,0 +1,23 @@
+// Kayley Seow
+// 9/7/2020
+// Lab 2
+// P2.20, This program  reads an integer and breaks it into a sequence of individual digits using spaces.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string number;
+    cout << "Enter a number:\n";
+    cin >> number;
+    int numlength = number.length();
+    for (int i = 1; i < numlength * 2 - 1; i+=2) 
+    {
+        string before = number.substr(0, i);
+        string after = number.substr(i);
+        number = before + " " + after;
+    }
+    cout << number + "\n";
+}
