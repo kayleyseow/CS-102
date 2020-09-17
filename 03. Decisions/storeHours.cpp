8fc6be2cbf3d9c6ab21efa906bea08edceac6f24
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-17 10:14:03 -0700

    🖥 Update storeHours.cpp
    Add the question prompt 📃

--- 03. Decisions/storeHours.cpp
+++ 03. Decisions/storeHours.cpp
@@ -1,3 +1,9 @@
+/*
+A store’s hours are Mon.–Fri. 8:00 a.m.–10:00 p.m., Saturday 10:00 a.m.–5:00 p.m., and Sunday 12:00 p.m.–5:00 p.m.  The user will type in a day, which will be stored in the 
+character variable dayName.  (You can assume it is a valid day.)  The values are S-Sunday, M-Monday, T-Tuesday, W-Wednesday, R-Thursday, F-Friday, A-Saturday.  Write an if 
+statement that will print a message giving the store’s hours.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
