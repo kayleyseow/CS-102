0485960c6d820d6326916c8f3fd59602fe6640e4
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-18 20:22:03 -0700

    🖥 Update userSizeAndHeight.cpp
    Add the prompt for the question

--- 03. Decisions/userSizeAndHeight.cpp
+++ 03. Decisions/userSizeAndHeight.cpp
@@ -3,6 +3,7 @@ The user entered T (for tall) or S (for short), and it was stored in the charact
 You should write an if that verifies the height and prints OK if the combination is valid or an error message if it is not.  To be valid, if the user entered T, the height should 
 be more than 72.  If the user entered S, the height should be less than or equal to 72.  Do this with one if using and, and else.
 */
+
 #include <iostream>
 #include <string>
 using namespace std;
