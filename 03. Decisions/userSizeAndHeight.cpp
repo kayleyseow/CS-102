cac2814fe059ff5a23b02a764f4b6c30362c28c7
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-18 20:18:00 -0700

    🖥 Create userSizeAndHeight.cpp

---
+++ 03. Decisions/userSizeAndHeight.cpp
@@ -0,0 +1,14 @@
+int main() {
+  char userSize;
+  cin >> userSize;
+  int height;
+  cin >> height;
+  if ((userSize =='T'&& height >72) || (userSize == 'S' && height <=72))
+  {
+    cout << "OK\n";
+  }
+  else
+  {
+    cout << "Error: your combination is invalid.\n";
+  }
+}
