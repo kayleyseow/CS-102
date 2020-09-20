13a202493d3e2aadf786ff06789eb26f6f824eec
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-19 21:07:01 -0700

    💻 Create P3_1.cpp

---
+++ 03. Decisions/P3_1.cpp
@@ -0,0 +1,34 @@
+int main() {
+    double tempVal; 
+    string tempType;
+    cout << "Enter the temperature value and the letter C for Celsius or F for Fahrenheit.\n";
+    cin >> tempVal >> tempType;
+    if (tempType == "C")
+    {
+        if (tempVal >= 100) {
+            cout << "Water is gaseous.\n";
+        }
+        else if (tempVal >= 0) 
+        {
+            cout << "Water is liquid.\n";
+        }
+        else // tempVal < 0
+        {
+            cout << "Water is solid.\n";
+        }
+    }
+    else
+    {
+        if (tempVal >= 212) {
+            cout << "Water is gaseous.\n";
+        }
+        else if (tempVal >= 32)
+        {
+            cout << "Water is liquid.\n";
+        }
+        else // tempVal < 32
+        {
+            cout << "Water is solid.\n";
+        }
+    }
+}
