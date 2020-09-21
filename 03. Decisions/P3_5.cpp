4ddf371d1c39d378e57dfa4cb29f3f407f2c0061
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-20 23:53:32 -0700

    💻 Update P3_5.cpp

--- 03. Decisions/P3_5.cpp
+++ 03. Decisions/P3_5.cpp
@@ -2,3 +2,5 @@
 // 9/20/2020
 // Lab 3
 // P3.5, This programm reads an integer and prints how many digits the number has, by checking whether the number is ≥ 10, ≥ 100, and so on.
+
+#include <iostream>
