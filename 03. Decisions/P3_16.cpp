64800e9b88fbe070ce1beb8f60cf93ca156fb9df
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-20 21:12:43 -0700

    💻 Create P3_16.cpp

---
+++ 03. Decisions/P3_16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name; 
+    double salary, hours, paycheck;
+    double overtime = 0;
+    cout << "Enter your name, salary, and hours worked.\n";
+    cin >> name >> salary >> hours;
+    if (hours > 40) 
+    {
+        overtime = (salary * 1.5 * (hours - 40));
+        hours = 40;
+    }
+    paycheck = (salary * hours) + overtime;
+    cout << name << "'s paycheck: \n" << name << " earned " << paycheck << endl;
+}
