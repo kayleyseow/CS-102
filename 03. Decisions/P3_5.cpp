2f649bf1b9b37be0880f8000864d1ccb68a98a14
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-20 23:58:32 -0700

    💻 Update P3_5.cpp

--- 03. Decisions/P3_5.cpp
+++ 03. Decisions/P3_5.cpp
@@ -6,3 +6,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter a number ≥ -1,000,000,000 and ≤ 1,000,000,000\n";
+    cin >> number;
+    if (number < 0)
+    {
+        number *= -1;
+    }
+    if (number < 10) cout << "This number has 1 digit.\n";
+    else if (number < 100) cout << "This number has 2 digits.\n";
+    else if (number < 1000) cout << "This number has 3 digits.\n";
+    else if (number < 10000) cout << "This number has 4 digits.\n";
+    else if (number < 100000) cout << "This number has 5 digits.\n";
+    else if (number < 1000000) cout << "This number has 6 digits.\n";
+    else if (number < 100000000) cout << "This number has 7 digits.\n";
+    else if (number < 1000000000) cout << "This number has 8 digits.\n";
+    else if (number < 10000000000) cout << "This number has 9 digits.\n";
+    else cout << "This number has 10 digits.\n";
