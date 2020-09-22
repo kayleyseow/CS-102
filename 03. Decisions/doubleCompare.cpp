32d72b06484d01e8da6b504d3a78ff0e52d8d808
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-09-22 14:58:46 -0700

    🖥 Create doubleCompare.cpp

---
+++ 03. Decisions/doubleCompare.cpp
@@ -0,0 +1,13 @@
+// Practice comparing doubles using EPSILON
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  const double EPSILON = 1E-14;
+  double r = sqrt(2.0);
+  if (abs(r*r-2)< EPSILON)
+  {
+    cout << "sqrt(2) squared is approximately 2.\n";
+  }
+}
